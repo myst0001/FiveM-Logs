4b9a1d1b457ea08b5d7d02208f3f080ba2589662
myst#0001 <82243276+myst0001@users.noreply.github.com>
2021-08-05 12:58:50 -0700

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,44 @@
+DiscordWebhookSystemInfos = ''
+DiscordWebhookKillinglogs = ''
+DiscordWebhookChat = ''
+
+SystemAvatar = ''
+
+UserAvatar = ''
+
+SystemName = 'myst'
+
+
+--[[ Special Commands formatting
+		 *YOUR_TEXT*			--> Make Text Italics in Discord
+		**YOUR_TEXT**			--> Make Text Bold in Discord
+	   ***YOUR_TEXT***			--> Make Text Italics & Bold in Discord
+		__YOUR_TEXT__			--> Underline Text in Discord
+	   __*YOUR_TEXT*__			--> Underline Text and make it Italics in Discord
+	  __**YOUR_TEXT**__			--> Underline Text and make it Bold in Discord
+	 __***YOUR_TEXT***__		--> Underline Text and make it Italics & Bold in Discord
+		~~YOUR_TEXT~~			--> Strikethrough Text in Discord
+]]
+-- Use 'USERNAME_NEEDED_HERE' without the quotes if you need a Users Name in a special command
+-- Use 'USERID_NEEDED_HERE' without the quotes if you need a Users ID in a special command
+
+
+-- These special commands will be printed differently in discord, depending on what you set it to
+SpecialCommands = {
+				--   {'/revive', '**[Admin Revive]:**'}
+				  }
+
+						
+-- These blacklisted commands will not be printed in discord
+BlacklistedCommands = {
+
+					  }
+
+-- These Commands will use their own webhook
+OwnWebhookCommands = {
+					 }
+
+-- These Commands will be sent as TTS messages
+TTSCommands = {
+			  }
+
